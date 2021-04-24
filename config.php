2d4b0055b09c41d0c576a1f40ed423b32fff6cfa
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-04-24 16:59:07 +0300

    Set config.php to uninstalled

--- config.php
+++ config.php
@@ -1,7 +1,2 @@
 <?php
-   $installed=TRUE;
-   $email="info@kaikkitietokoneista.net";
-   $database_config = array(
-       'database_type' => 'sqlite',
-       'database_file' => './database.db'
-   );
+   $installed=FALSE;
